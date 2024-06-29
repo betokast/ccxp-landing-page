83991fce448c4299651addf55e1bae64398b844e
robertoCastro <bkastro@gmail.com>
2024-06-29 00:48:11 -0300

    Implementação de função para habilitar os cards através do teclado (1 a 4)

--- scripts/script.js
+++ scripts/script.js
@@ -33,3 +33,56 @@ setInterval(countdown, 1000);
 function highlightCard(selector) {
   let element = document.querySelector(selector);
   element.classList.toggle('card-highlight');}
+
+
+// Função para identificar tecla pressionada
+function checkKeyboardCode() {
+  document.addEventListener('keydown', (event) => {
+    let nome = event.key;
+    let code = event.code;
+    alert(`Tecla pressionada ${nome} \r\n Key Code: ${code}`);
+  }, false);
+}
+
+// Função para reagir a uma tecla pressionada
+function addKeyboardEventListener() {
+  document.addEventListener('keydown', (event) => {
+    var ingresso1 = document.getElementById("quinta");
+    var ingresso2 = document.getElementById("sexta");
+    var ingresso3 = document.getElementById("sabado");
+    var ingresso4 = document.getElementById("domingo");
+
+    var code = event.code;
+    if(code == 'Digit1' || code == 'Numpad1') {
+      ingresso1.classList.toggle("card-highlight");
+      ingresso2.classList.remove("card-highlight");
+      ingresso3.classList.remove("card-highlight");
+      ingresso4.classList.remove("card-highlight");
+    }
+
+    if(code == 'Digit2' || code == 'Numpad2') {
+      ingresso1.classList.remove("card-highlight");
+      ingresso2.classList.toggle("card-highlight");
+      ingresso3.classList.remove("card-highlight");
+      ingresso4.classList.remove("card-highlight");
+    }
+
+    if(code == 'Digit3' || code == 'Numpad3') {
+      ingresso1.classList.remove("card-highlight");
+      ingresso2.classList.remove("card-highlight");
+      ingresso3.classList.toggle("card-highlight");
+      ingresso4.classList.remove("card-highlight");
+    }
+
+    if(code == 'Digit4' || code == 'Numpad4') {
+      ingresso1.classList.remove("card-highlight");
+      ingresso2.classList.remove("card-highlight");
+      ingresso3.classList.remove("card-highlight");
+      ingresso4.classList.toggle("card-highlight");
+    }
+  }, false);
+}
+
+
+// checkKeyboardCode();
+addKeyboardEventListener();
